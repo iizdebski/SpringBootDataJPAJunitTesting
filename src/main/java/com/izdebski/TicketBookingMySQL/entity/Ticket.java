b61d0fb6fb5bba9a655d03c1bb2711b299adf7dc
Ivan Izdepskyi <iizdepskyy112@gmail.com>
2019-01-02 16:34:54 +0200

    Integration tests

--- src/main/java/com/izdebski/TicketBookingMySQL/entity/Ticket.java
+++ src/main/java/com/izdebski/TicketBookingMySQL/entity/Ticket.java
@@ -1,13 +1,8 @@
 package com.izdebski.TicketBookingMySQL.entity;
 
+import javax.persistence.*;
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
 @Table(name="ticket")
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,8 +9,8 @@ CREATE TABLE ticket
   ticket_Id      INT AUTO_INCREMENT
     PRIMARY KEY,
   passenger_name VARCHAR(100) NOT NULL,
-  booking_date   TIMESTAMP    NOT NULL,
   source_station VARCHAR(100) NOT NULL,
+  booking_date   TIMESTAMP NOT NULL,
   dest_station   VARCHAR(100) NOT NULL,
   email VARCHAR (100) NOT NULL,
   CONSTRAINT ticket_id_UNIQUE
